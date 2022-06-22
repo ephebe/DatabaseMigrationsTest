a302c2dc1fd0829e0af13e013350f415668cf8e6
Hugo <hugo@super-royal.com>
2022-06-22 11:30:33 +0800

    增加Sql Server Database Project

---
+++ DatabaseMigrationsTest.SqlServerDataTools/Tables/Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[PersonId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NCHAR(500) NOT NULL, 
+    [LastName] NCHAR(255) NOT NULL, 
+    [DateOfBirth] DATE NOT NULL
+)
